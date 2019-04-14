7a7cc40e7d5ace801d92c6827353a5fe07928796
GabrielMottaDev <gabrielmottadev@gmail.com>
2019-04-14 02:44:44 -0300

    Update

--- Revisa/src/one/attom/revisa/Revisa.java
+++ Revisa/src/one/attom/revisa/Revisa.java
@@ -7,6 +7,28 @@ import java.util.Scanner;
  * @since 13/04/2019
  */
 public class Revisa {
+    
+     public static boolean isBisexto(int ano){
+        if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean iAAsPrimo(int numero) {
+        /* Numero primo é apenas divisivel por 1 e por ele mesmo,
+         * então o for vai de 2 até um numero antes dele mesmo e verifica se é divisivel,
+         * se for ele não é primo.
+         * Obs: E já como todo numero é divido por 1 ou por ele mesmo isso nem foi considerado */
+        for (int divi = 2; divi < numero; divi++) {
+            if (numero % divi == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         int numA = 1, numB = 1;
@@ -16,8 +38,6 @@ public class Revisa {
         long longA = 42424242424242L;
         char charA = 'a';
         String texto = "Teste";
-
-        Scanner input = new Scanner(System.in);
 
         System.out.println("O valor de doubleA é: " + doubleA);
         System.out.println("Digite um ano: ");
@@ -45,24 +65,4 @@ public class Revisa {
         System.out.println("Numeros primos: " + numerosPrimos);
     }
     
-    public static boolean isBisexto(int ano){
-        if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isPrimo(int numero) {
-        /* Numero primo é apenas divisivel por 1 e por ele mesmo,
-         * então o for vai de 2 até um numero antes dele mesmo e verifica se é divisivel,
-         * se for ele não é primo.
-         * Obs: E já como todo numero é divido por 1 ou por ele mesmo isso nem foi considerado */
-        for (int divi = 2; divi < numero; divi++) {
-            if (numero % divi == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
